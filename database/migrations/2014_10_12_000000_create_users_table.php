a38375a581cdc41e44e181dcc0a190e3c64b0619
Bryan Arévalo <barevalo@solcomp.com>
2022-09-20 13:15:05 -0600

    :rocket: Cambiadas las migraciones

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,8 +21,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->integer('coins')->default(0);
-            $table->longText('img')->nullable();
+            $table->string('role');
         });
     }
 
