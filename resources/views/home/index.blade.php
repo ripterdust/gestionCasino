56e10c831b118c699cb7eef19d9d29206ee089be
Bryan Arévalo <barevalo@solcomp.com>
2022-09-19 22:03:50 -0600

    :rocket: Agregados los estilos principales a la navbar

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,13 +4,15 @@
 
 @section('content')
 
+    <div class="centro">
 
-    <div style="width:500px;" id="reader"></div>
+        <div style="width:500px;" id="reader"></div>
 
-    
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5-qrcode/2.2.1/html5-qrcode.min.js" integrity="sha512-cuVnjPNH3GyigomLiyATgpCoCmR9T3kwjf94p0BnSfdtHClzr1kyaMHhUmadydjxzi1pwlXjM5sEWy4Cd4WScA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script>
-        const URLQR = {!! json_encode(route('login_qr', ['usuario' => '__usuario', 'id' => '__id'])) !!}
-    </script>
+        
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/html5-qrcode/2.2.1/html5-qrcode.min.js" integrity="sha512-cuVnjPNH3GyigomLiyATgpCoCmR9T3kwjf94p0BnSfdtHClzr1kyaMHhUmadydjxzi1pwlXjM5sEWy4Cd4WScA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script>
+            const URLQR = {!! json_encode(route('login_qr', ['usuario' => '__usuario', 'id' => '__id'])) !!}
+        </script>
+    </div>
     @vite('resources/js/qr.js')
 @endsection
