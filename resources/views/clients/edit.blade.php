56694c90089bb753298d910c458bc122a3a69ea4
Bryan Arévalo <barevalo@solcomp.com>
2022-09-22 21:45:33 -0600

    :rocket: Agregados los formularios para la creación de los usuarios

--- resources/views/clients/edit.blade.php
+++ resources/views/clients/edit.blade.php
@@ -31,16 +31,21 @@
                 <input type="text" name="lname" required value="{{ $cliente->lname }}">
             </div>
             <div class="frm">
+                <label for="">Fecha de nacimiento</label>
+                <input type="date" name="birth" required value="{{ $cliente->birth }}">
+            </div>
+             <div class="frm">
+                <label for="">Dirección</label>
+                <input type="email" name="adress" requried value="{{ $cliente->adress }}">
+            </div>
+            <div class="frm">
                 <label for="">Correo electrónico</label>
                 <input type="email" name="email" requried value="{{ $cliente->email }}">
             </div>
-            <div class="frm">
-                <label for="">Fecha de nacimiento</label>
-                <input type="text" name="name" required value="{{ $cliente->birth }}">
-            </div>
+            
             <div class="frm">
                 <label for="">Celular</label>
-                <input type="text" name="name" required value="{{ $cliente->phone }}">
+                <input type="text" name="phone" required value="{{ $cliente->phone }}">
             </div>
 
             <input type="submit" value="Guardar" class="btn btn-fill">
