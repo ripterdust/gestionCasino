e125a7025c5f01ae5e27a77b13d7c241ba9ce53b
Bryan Arévalo <barevalo@solcomp.com>
2022-09-25 11:02:20 -0600

    :rocket: Agregados los estilos de home

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/js/app.js', 'resources/sass/index.scss'])
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+
 
     <title>@yield('title')</title>
 </head>
--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    @vite(['resources/js/app.js', 'resources/sass/index.scss'])
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
     <title>@yield('title')</title>
 </head> 
 <body class="body">
