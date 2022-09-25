51ebc7c24e71de52258063008aa0f8ce5ceb3ac9
Bryan Arévalo <barevalo@solcomp.com>
2022-09-25 11:43:04 -0600

    :rocket: eliminado vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    {{-- <link rel="stylesheet" href="{{ asset('css/index.css') }}"> --}}
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
        <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
     />
-    @vite(['resources/js/app.js', 'resources/sass/index.scss'])
+    {{-- @vite(['resources/js/app.js', 'resources/sass/index.scss']) --}}
 
     <title>@yield('title')</title>
 </head>
