9535fa75e8020f1593acdc757d856455d56e1bf3
Bryan Arévalo <barevalo@solcomp.com>
2022-09-25 09:43:33 -0600

    :rocket: Agregados los estilos de las métricas

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/auth.css') }}">
-        @vite(['resources/js/app.js', 'resources/sass/auth/index.scss'])
+    @vite(['resources/js/app.js', 'resources/sass/index.scss'])
+
     <title>@yield('title')</title>
 </head>
-<body>
+<body class="body">
     @yield('content')
 </body>
 </html>
