98065dde6392de604ab27acaf45c9a9d16a92f9a
Bryan Arévalo <barevalo@solcomp.com>
2022-09-24 19:01:52 -0600

    :rocket: Eliminado el appjs

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -6,7 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
      <link rel="stylesheet" href="{{ asset('css/common.css') }}">
-     <script src="{{ asset('js/app.js') }}"></script>
     {{-- @vite(['resources/js/app.js', 'resources/sass/common/index.scss']) --}}
     <title>@yield('title')</title>
 </head> 
