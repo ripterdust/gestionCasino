712a02625aab53a953bdec5dab2f2ee911af6513
Bryan Arévalo <barevalo@solcomp.com>
2022-09-27 10:24:15 -0600

    :rocket: Agregado el favicon y la imagen

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/index.css') }}">
-
+    <link rel="icon" type="image/x-icon" href="{{ asset('/img/favicon.ico') }}">
     {{-- @vite(['resources/js/app.js', 'resources/sass/index.scss']) --}}
 
     <title>@yield('title')</title>
